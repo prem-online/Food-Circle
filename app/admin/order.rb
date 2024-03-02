ccbb3fb71002d623098bc22b92b1dbce21a2c17b
prem-online <premkaithwas19@gmail.com>
2024-03-02 20:54:30 +0530

    Final touches

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,55 +1,55 @@
 # frozen_string_literal: true
+
 ActiveAdmin.register Order do
-    menu label: "Orders"
-    permit_params :order_number, :total, order_items_attributes: [:id, :product_id, :quantity, :_destroy]
-    filter :products
-    filter :total
-    filter :created_at
+  menu label: 'Orders'
+  permit_params :order_number, :total, order_items_attributes: %i[id product_id quantity _destroy]
+  filter :products
+  filter :total
+  filter :created_at
 
+  index title: 'Order List' do
+    selectable_column
+    column :order_number
+    column :products do |order|
+      order.products.each do |product|
+        product.name
+      end
+    end
+    column :total
+    column  'Order Date', :created_at do |object|
+      object.created_at.strftime('%e %b at %I:%M%p, %Y')
+    end
+    actions
+  end
 
-        index title: 'Order List' do
-          selectable_column
-            column :order_number
-            column :products do |order|
-                order.products.each do |product|
-                    product.name
-                end
-            end
-            column :total
-            actions
+  show do
+    panel 'Products' do
+      table_for order.order_items do
+        column :name do |oi|
+          oi.product.name
         end
+        column :item_price
+        column :quantity
+      end
+    end
 
-        show do
-            panel "Products" do
-              table_for order.order_items do
-                column :name do |oi|
-                  oi.product.name
-                end
-                column :item_price
-                column :quantity
-              end
-            end
-            
-            attributes_table do
-              row :total do
-                  order.total
-              end
-              row :date do
-                order.created_at.strftime('%e %b %Y')
-              end
-            end
-        end
+    attributes_table do
+      row :total do
+        order.total
+      end
+      row :date do
+        order.created_at.strftime('%e %b %Y')
+      end
+    end
+  end
 
-
-        form do |f|
-            f.has_many :order_items, heading: false, allow_destroy: true, id: 'Love' do |item|
-              item.input :product
-              item.input :quantity
-              item.input :_destroy, as: :boolean, required: false, label: 'Remove item'
-              # Add other order item attributes as needed
-            end
-          f.actions
-        end
-        
+  form do |f|
+    f.has_many :order_items, heading: false, allow_destroy: true, id: 'Love' do |item|
+      item.input :product
+      item.input :quantity
+      item.input :_destroy, as: :boolean, required: false, label: 'Remove item'
+      # Add other order item attributes as needed
+    end
+    f.actions
+  end
 end
-  
