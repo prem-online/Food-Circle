993f3fbb75674dcb8cb3725ca91e95c9ce0bd305
prem-online <premkaithwas19@gmail.com>
2024-03-04 20:55:09 +0530

    Add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.hosts << "eatery-manager.onrender.com"
   # Enable server timing
   config.server_timing = true
 
