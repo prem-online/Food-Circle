b5caa530578c731094a49e3720d4239cc3289799
prem-online <premkaithwas19@gmail.com>
2024-08-17 17:24:59 +0530

    Enhance new order page

--- app/javascript/components/Orders/NewOrder.js
+++ app/javascript/components/Orders/NewOrder.js
@@ -6,9 +6,10 @@ import axios from 'axios';
 
 import UserDashboard from '../dashboard/UserDashboard';
 import NewOrderSkeleton from './NewOrderSkeleton';
-
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import { useLogin } from '../../helpers/useLogin';
-
+import {COLORS} from '../../helpers/colors';
 import { BASE_URL, MODULE_SUCCESSFULL_CREATED } from '../../constants';
 
 import { sleep, extractInteger, transformData } from '../../helpers/common';
@@ -59,14 +60,24 @@ const NewOrder = () => {
   }
 
   const addQuantity = (id, quantity) => {
-    if (Object.keys(productQuantity).length === 0){
-      setProductQuantity({ [`pd-${id}`]: quantity })
-    }else{
-      const newQuantity = productQuantity[`pd-${id}`] === undefined ? quantity : productQuantity[`pd-${id}`]+quantity
-      let obj = productQuantity;
-      obj[`pd-${id}`] = newQuantity;
-      setProductQuantity(obj);
+    const isItFirstCall = Object.keys(productQuantity).length === 0
+    let finalQuantityObj = {}
+
+    if (isItFirstCall && quantity === 3){
+      return
     }
+    else if (!isItFirstCall && quantity === 3){
+      const newQuantity = productQuantity[`pd-${id}`] === undefined ? quantity : productQuantity[`pd-${id}`]-1
+      finalQuantityObj = productQuantity;
+      finalQuantityObj[`pd-${id}`] = newQuantity == 0 ? 0 : newQuantity;
+    }
+  else{
+      const newQuantity = productQuantity[`pd-${id}`] === undefined ? quantity : productQuantity[`pd-${id}`]+quantity
+      finalQuantityObj = productQuantity;
+      finalQuantityObj[`pd-${id}`] = newQuantity;
+    }
+    
+    setProductQuantity(finalQuantityObj);
     showQuantity(id,"addQuantity")
   }
  
@@ -162,10 +173,10 @@ const NewOrder = () => {
                     <Stack mt={1}>
                       <Stack direction="row" spacing={2}>
                       <Fab ml={2} color="primary" aria-label="add" onClick={()=> {addQuantity(row.id,1)}}>
-                        +1
+                        <AddIcon sx={{ color: COLORS.WHITE }}/>
                       </Fab>
                       <Fab color="primary" aria-label="add" onClick={()=> {addQuantity(row.id,3)}}>
-                        +3
+                        <RemoveIcon sx={{color: COLORS.WHITE}} />
                       </Fab>
                       <Fab color="primary" aria-label="add" onClick={()=> {addQuantity(row.id,10)}}>
                         +10
--- app/javascript/constants.js
+++ app/javascript/constants.js
@@ -4,33 +4,10 @@ export const REFRESH_TOKEN_URL = `${BASE_URL}/api/v1/logins/refresh`
 
 // HELPERS
 export const MAX_RESULTS = 10;
-export const COLORS = {
-  PRIMARY: '#3498db',
-  SECONDARY: '#2ecc71',
-  BLACK: '#121212',
-};
 
 // MESSAGES
 export const MODULE_SUCCESSFULL_CREATED = 'created successfully.';
 export const LOGGED_IN_SUCCESSFULLY = 'Logged in successfully.';
-
-export const BUTTONS = {
-  PRIMARY: {
-    fontWeight: "bold", 
-    margin: 0,
-    fontSize: "1.25rem",
-    letterSpacing: 1,
-    fontFamily: "sans-serif roboto",
-    lineHeight: 1.6,
-    textTransform: "capitalize",
-    marginBottom: 2,
-    color: COLORS.BLACK,
-  },
-  SECONDARY: {
-    color: COLORS.WHITE,
-    backgroundColor: COLORS.SECONDARY,
-  },
-}
 
 export const V2 = BASE_URL+"api/v2/";
 
